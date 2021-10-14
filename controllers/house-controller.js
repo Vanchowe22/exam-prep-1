8360ecac85560a23ce42f5464a4aa2997f8f784f
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2021-10-14 15:45:16 +0300

    rendert details

--- controllers/house-controller.js
+++ controllers/house-controller.js
@@ -3,7 +3,7 @@ const houseService = require('../service/house-service');
 
 const renderCreate = (req, res) => {
     res.render('house/create')
-}
+};
 
 const postCreate = async (req, res) => {
     let body = req.body;
@@ -11,11 +11,22 @@ const postCreate = async (req, res) => {
     await houseService.create(body, req.user.id)
     
     res.redirect('/')
-}
+};
 
 const renderRentHouse = async (req, res) => {
     let data = await houseService.getAll();
     res.render('house/aprt-for-recent', { data });
+};
+
+const renderDetails = (req, res) => {
+    houseService.getOne(req.params.id)
+        .then((data) => res.render('details', data))
+}; 
+
+const renderEdit = async (req, res) => {
+    let data = await houseService.getOne(req.params.id);
+
+    res.render('house/edit', data)
 }
 
 router.route('/create')
@@ -24,5 +35,12 @@ router.route('/create')
 
 router.route('/rent')
     .get(renderRentHouse)
+
+router.route('/:id/details')
+    .get(renderDetails)
+
+router.route('/:id/edit')
+    .get()
+
 
 module.exports = router;
--- service/house-service.js
+++ service/house-service.js
@@ -3,8 +3,11 @@ const House = require('../models/House');
 const create = ({ name, type, year, city, homeImage, description, availablePieces, }, _owner) => House.create({ name, type, year, city, homeImage, description, availablePieces, _owner });
 
 const getAll = () => House.find().lean();
+const getOne = (id) => House.findById(id).populate().lean();
 
 module.exports = {
     create,
     getAll,
+    getOne,
+    
 }
